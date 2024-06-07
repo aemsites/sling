f431bc36732262431bd90cc54732c9b4998e3d79
Amol Anand <amol.anand@gmail.com>
2024-06-07 00:47:07 -0400

    working category and mobile styles and import tweaks

--- aemedge/scripts/utils.js
+++ aemedge/scripts/utils.js
@@ -1,4 +1,6 @@
-import { getMetadata, buildBlock, decorateBlock } from './aem.js';
+import {
+  getMetadata, buildBlock, decorateBlock, createOptimizedPicture,
+} from './aem.js';
 
 export const PRODUCTION_DOMAINS = ['sling.com'];
 const domainCheckCache = {};
@@ -176,4 +178,133 @@ export function getPageType() {
   const template = getMetadata('template');
   if (template === 'blog-article' && !category) return 'blog';
   return 'category';
+}
+
+/**
+ * Fetches and transforms data from a JSON file
+ * @param {string} path - The path to the JSON file
+ * @returns {Promise<Array>} - A promise resolving to the transformed data array
+ */
+async function fetchData(path) {
+  const response = await fetch(path);
+  const json = await response.json();
+
+  return json.data.map((row) => {
+    if (row.image.startsWith('/default-meta-image.png')) {
+      row.image = `/${window.hlx.codeBasePath}${row.image}`;
+    }
+    return row;
+  });
+}
+
+/**
+ * Converts excel date into JS date.
+ * @param {number} excelDate date to convert.
+ */
+export function convertExcelDate(excelDate) {
+  const jsDate = +excelDate > 99999
+    ? new Date(+excelDate * 1000)
+    : new Date(Math.round((+excelDate - (1 + 25567 + 1)) * 86400 * 1000));
+  return jsDate;
+}
+
+function compareArrays(arr, arr2) {
+  return arr.every((i) => arr2.includes(i));
+}
+
+/**
+ * Retrieves blogs matching specific tags
+ * @param {Array} categories - An array of categories to filter by
+ * @param {number} num - The number of blogs to retrieve
+ * @returns {Promise<Array>} - A promise resolving to the filtered blogs array
+ */
+export async function getBlogs(categories, num) {
+  if (!window.allBlogs) {
+    window.allBlogs = await fetchData('/whatson/query-index.json');
+  }
+
+  const blogArticles = window.allBlogs.filter((e) => e.category === '');
+  if (categories && categories.length > 0) {
+    const filteredList = blogArticles.filter((e) => {
+      const rawTags = JSON.parse(e.tags);
+      const tags = rawTags.map((tag) => tag.trim().toLowerCase());
+      return compareArrays(categories, tags);
+    });
+    if (num) {
+      return filteredList.slice(0, num);
+    }
+    return filteredList;
+  }
+  return [];
+}
+
+/**
+ * Creates a card element from an index row
+ * @param {Object} row - JSON Object representing a blog
+ * @param {String} style - The style of the card (default: 'card')
+ * @param {Boolean} eagerImage - Whether to load the image eagerly (default: false)
+ * @param {Boolean} fullCardLink - Whether to set the link over the entire card (default: false)
+ * @returns {Promise<Array>} - A promise resolving to the card element
+ */
+export async function createCard(row, style, eagerImage = false, fullCardLink = false) {
+  // Create card div
+  const card = createTag('div', { class: style || 'card' });
+
+  // Create and add the link
+  const link = createTag('a', { class: 'card-link', href: row.path, alt: row.title });
+
+  // Add the image to the link and then card first
+  if (row.image !== '0' && row.title !== '0') {
+    const cardImage = createTag('div', { class: 'card-image' });
+    cardImage.append(createOptimizedPicture(row.image, row.title, eagerImage));
+    link.append(cardImage);
+    card.prepend(link);
+  }
+
+  // Create a separate child div for the card content
+  const cardContent = createTag('div', { class: 'card-content' });
+
+  // Add tags
+  if (row.tags && row.tags !== '0') {
+    const tags = createTag('div', { class: 'card-tags' });
+    const tagArray = JSON.parse(row.tags);
+    tagArray.forEach((tag) => {
+      const tagElement = createTag('a', { class: 'card-tag-link', href: `/whatson/${tag.toLowerCase()}` }, tag.toUpperCase());
+      tags.append(tagElement);
+    });
+    cardContent.append(tags);
+  }
+  // Add title
+  if (row.title && row.title !== '0') {
+    const title = createTag('div', { class: 'card-title' }, row.title);
+    cardContent.append(title);
+  }
+  // Add description
+  if (row.description && row.description !== '0') {
+    const description = createTag('div', { class: 'card-description' }, `${row.description.substring(0, 100)}...`);
+    cardContent.append(description);
+  }
+  // Add author and publish date
+  const authorDate = createTag('div', { class: 'card-author-date' });
+  let author;
+  if (!row.author || row.author === '0') {
+    author = createTag('span', { class: 'card-author' }, 'Sling Staff');
+  } else {
+    author = createTag('span', { class: 'card-author' }, row.author);
+  }
+  authorDate.append(author);
+  if (row.date && row.date !== '0') {
+    const publishDate = convertExcelDate(row.date);
+    const dateOptions = { year: 'numeric', month: 'long', day: 'numeric' };
+    const formattedDate = publishDate.toLocaleDateString('en-US', dateOptions);
+    const date = createTag('span', { class: 'card-date' }, formattedDate);
+    authorDate.append(date);
+  }
+  cardContent.append(authorDate);
+  if (fullCardLink) {
+    link.append(cardContent);
+  } else {
+    card.append(cardContent);
+  }
+  return (card);
 }
