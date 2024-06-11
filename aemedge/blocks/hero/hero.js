1d66bce6298ad57673219276511ff4057be3d644
Amol Anand <amol.anand@gmail.com>
2024-06-11 18:53:17 -0400

    multiple bg images in hero

--- aemedge/blocks/hero/hero.js
+++ aemedge/blocks/hero/hero.js
@@ -5,6 +5,25 @@ export default function decorate(block) {
   const h1Parent = block.querySelector('h1').parentElement;
   h1Parent.classList.add('hero-content');
 
+  // Handle multiple images
+  const pictures = block.querySelectorAll('picture');
+  if (pictures.length > 1) {
+    pictures.forEach((picture, i) => {
+      if (picture.parentElement.tagName === 'P') {
+        picture.parentElement.remove();
+      }
+      if (i === 0) {
+        picture.classList.add('background');
+        h1Parent.parentElement.prepend(picture);
+        h1Parent.parentElement.classList.add('multiple-images');
+      }
+      if (i !== 0) {
+        picture.classList.add('foreground');
+        h1Parent.append(picture);
+      }
+    });
+  }
+
   // Handle video
   const videoLink = block.querySelector('a[href*=".mp4"]');
   if (videoLink) {
