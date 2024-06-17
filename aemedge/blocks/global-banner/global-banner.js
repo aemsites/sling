b9d213e76be7367e0522ca96bf7500281ec99bce
Amol Anand <amol.anand@gmail.com>
2024-06-17 13:23:28 -0400

    fixed styling

--- aemedge/blocks/global-banner/global-banner.js
+++ aemedge/blocks/global-banner/global-banner.js
@@ -1,7 +1,9 @@
 import { createTag } from '../../scripts/utils.js';
 
 export default async function decorate(block) {
+  const emptySpan = createTag('span', { class: 'empty' });
   const closeButton = createTag('span', { class: 'close' }, '×');
+  block.prepend(emptySpan);
   block.appendChild(closeButton);
 
   closeButton.addEventListener('click', () => {
