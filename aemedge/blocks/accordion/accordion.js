59f9569a32e2a56b64e5daff58076f2d453674f1
Rusmeen Khan <rusmeenk@Rusmeens-MacBook-Pro.local>
2024-07-10 13:37:41 +0530

    Added animation to faq accordion

--- aemedge/blocks/accordion/accordion.js
+++ aemedge/blocks/accordion/accordion.js
@@ -14,8 +14,8 @@ export default function decorate(block) {
   [...block.children].forEach((row) => {
     // decorate accordion item label
     const label = row.children[0];
-    const summary = document.createElement('summary');
-    summary.className = 'accordion-item-label';
+    const summary = document.createElement('div');
+    summary.className = 'summary accordion-item-label';
     summary.append(...label.childNodes);
     if (!hasWrapper(summary)) {
       summary.innerHTML = `<p>${summary.innerHTML}</p>`;
@@ -27,9 +27,17 @@ export default function decorate(block) {
       body.innerHTML = `<p>${body.innerHTML}</p>`;
     }
     // decorate accordion item
-    const details = document.createElement('details');
-    details.className = 'accordion-item';
+    const details = document.createElement('div');
+    details.classList.add('details', 'accordion-items');
     details.append(summary, body);
     row.replaceWith(details);
+  });
+
+  const questions = block.querySelectorAll('.accordion-items .summary');
+  questions.forEach((item) => {
+    item.addEventListener('click', () => {
+      const answer = item.parentElement;
+      answer.classList.toggle('open');
+    });
   });
 }
