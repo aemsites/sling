bbf1ff74704f193363804bd10735850e5f5d69c4
Ritwik Srivastava <ritwik@Ritwiks-MacBook-Pro-2.local>
2024-09-03 15:23:38 +0530

    Add header changes

--- aemedge/blocks/header/header.js
+++ aemedge/blocks/header/header.js
@@ -126,7 +126,21 @@ function moveTopNav(e, topNavSection, navSections) {
 export default async function decorate(block) {
   // load nav as fragment
   const navMeta = getMetadata('nav');
-  const navPath = navMeta ? new URL(navMeta).pathname : '/aemedge/nav';
+  let navPath = '/aemedge/nav';
+  const headerType = getMetadata('headertype');
+  if (headerType === 'header1') {
+    navPath = '/drafts/ritwik/header1';
+    /*const headerWrapper = document.querySelector('.header-wrapper');
+    // Remove the 'height' property from the element's inline styles
+    if (headerWrapper) {
+      headerWrapper.style.removeProperty('height');
+    }*/
+  } else if ( headerType === 'header2' ) {
+    navPath = '/drafts/ritwik/header2';
+  }
+  else {
+        navPath = navMeta ? new URL(navMeta).pathname : '/aemedge/nav';
+    }
   const fragment = await loadFragment(navPath);
   let topnavSection;
   if (fragment.childElementCount === 4) {
