e17cfea16dce3b77949fadec13c68ee6b58d1cf4
Kiran Murugulla <murugull@adobe.com>
2024-09-17 10:43:34 -0400

    try loading images lazily

--- aemedge/blocks/marquee/marquee.js
+++ aemedge/blocks/marquee/marquee.js
@@ -123,6 +123,7 @@ function processBlockConfig(block) {
 }
 
 export default function decorate(block) {
+  block.querySelectorAll('img').forEach((img) => { img.loading = 'lazy'; });
   processBlockConfig(block);
   const background = block.querySelector('.background');
   const bgColor = block.querySelector('.background-color');
