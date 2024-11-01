0499e35defc3536b1de54c42da5e54688a6095a1
Kiran Murugulla <murugull@adobe.com>
2024-11-01 13:30:27 -0400

    reverting changes

--- aemedge/scripts/utils.js
+++ aemedge/scripts/utils.js
@@ -237,15 +237,11 @@ export async function getBlogs(categories, num, limit = '') {
   const blogArticles = window.allBlogs.filter(
     (e) => (e.template === 'blog-article' && e.image !== '' && !e.image.startsWith('//aemedge/default-meta-image.png')),
   );
-  // If page is the home page, omit international tagged pages
-  // const homeBlogPage = window.location.pathname === '/whatson';
+
   if ((categories && categories.length > 0)) {
     const filteredList = blogArticles.filter((e) => {
       const rawTags = JSON.parse(e.tags);
       const tags = rawTags.map((tag) => tag.trim().toLowerCase());
-      // if (homeBlogPage) {
-      //  return !tags.includes('international');
-      // }
       return compareArrays(categories, tags);
     });
     if (num) {
