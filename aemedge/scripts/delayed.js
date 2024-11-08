8e62ca5f3ee813dd9213e1ff7429bf50b4351afa
Kiran Murugulla <murugull@adobe.com>
2024-11-08 17:54:49 -0500

    able to highlight blocks

--- aemedge/scripts/delayed.js
+++ aemedge/scripts/delayed.js
@@ -22,9 +22,45 @@ sampleRUM('cwv');
 // if they need it, uncomment it after go live.
 
 if (window.location.host.startsWith('localhost')) {
-  loadScript('https://assets.adobedtm.com/f4211b096882/26f71ad376c4/launch-b69ac51c7dcd-development.min.js');
+  // loadScript('https://assets.adobedtm.com/f4211b096882/26f71ad376c4/launch-b69ac51c7dcd-development.min.js');
 } else if (window.location.host.startsWith('www.sling.com') || window.location.host.endsWith('.live')) {
   loadScript('https://assets.adobedtm.com/f4211b096882/26f71ad376c4/launch-c846c0e0cbc6.min.js');
 } else if (window.location.host.endsWith('.page')) {
   loadScript('https://assets.adobedtm.com/f4211b096882/26f71ad376c4/launch-6367a8aeb307-staging.min.js');
+}
+
+const showBlocks = ({ detail: payload }) => {
+  console.log('a custom event happened', payload);
+  const blocks = document.querySelectorAll('div.block');
+
+  blocks.forEach((block) => {
+    block.classList.toggle('highlight');
+    let blockName = block.parentElement.querySelector('.blockname');
+    if (!blockName) blockName = document.createElement('span');
+    if (block.classList.contains('highlight')) {
+      blockName.classList.add('blockname');
+      // eslint-disable-next-line prefer-destructuring
+      blockName.innerText = block.className.split(' ')[0];
+      blockName.classList.toggle('show');
+      block.parentElement.prepend(blockName);
+    } else {
+      blockName.remove();
+    }
+  });
+
+  const sections = document.querySelectorAll('div.section');
+  sections.forEach((section) => section.classList.toggle('highlight'));
+};
+
+const sk = document.querySelector('helix-sidekick');
+if (sk) {
+  // sidekick already loaded
+  sk.addEventListener('custom:showblocks', showBlocks);
+} else {
+  // wait for sidekick to be loaded
+  document.addEventListener('sidekick-ready', () => {
+    // sidekick now loaded
+    document.querySelector('helix-sidekick')
+      .addEventListener('custom:showblocks', showBlocks);
+  }, { once: true });
 }
