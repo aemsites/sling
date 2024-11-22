5b295fcb4de2066cb4c064813ccc3b9ad923dc9c
Kiran Murugulla <murugull@adobe.com>
2024-11-22 10:46:33 -0500

    loading the martech lazily got perf. score down by 26 points on mobile

--- aemedge/scripts/scripts.js
+++ aemedge/scripts/scripts.js
@@ -600,18 +600,6 @@ async function loadLazy(doc) {
   sampleRUM('lazy');
   sampleRUM.observe(main.querySelectorAll('div[data-block-name]'));
   sampleRUM.observe(main.querySelectorAll('picture > img'));
-
-  // loading the analytics, target libaries
-  await loadScript('/aemedge/scripts/sling-martech/analytics-lib.js');
-
-  if (window.location.host.startsWith('localhost')) {
-    // await loadScript('https://assets.adobedtm.com/f4211b096882/26f71ad376c4/launch-b69ac51c7dcd-development.min.js');
-    await loadScript('https://assets.adobedtm.com/f4211b096882/26f71ad376c4/launch-c846c0e0cbc6.min.js');
-  } else if (window.location.host.startsWith('www.sling.com') || window.location.host.endsWith('.live')) {
-    await loadScript('https://assets.adobedtm.com/f4211b096882/26f71ad376c4/launch-c846c0e0cbc6.min.js');
-  } else if (window.location.host.endsWith('.page')) {
-    await loadScript('https://assets.adobedtm.com/f4211b096882/26f71ad376c4/launch-6367a8aeb307-staging.min.js');
-  }
 }
 
 /**
