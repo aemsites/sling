d228db33944dc534040c9311ea4447cf058778e8
Kiran Murugulla <murugull@adobe.com>
2024-12-10 14:03:47 -0500

    ignoring aem-martech from lint checking

--- aemedge/scripts/delayed.js
+++ aemedge/scripts/delayed.js
@@ -3,13 +3,3 @@ import { sampleRUM } from './aem.js';
 
 // Core Web Vitals RUM collection
 sampleRUM('cwv');
-// load sling martech load utility and launch scripts
-// await loadScript('/aemedge/scripts/sling-martech/analytics-lib.js');
-
-// if (window.location.host.startsWith('localhost')) {
-//  await loadScript('https://assets.adobedtm.com/f4211b096882/26f71ad376c4/launch-b69ac51c7dcd-development.min.js');
-// } else if (window.location.host.startsWith('www.sling.com') || window.location.host.endsWith('.live')) {
-//  await loadScript('https://assets.adobedtm.com/f4211b096882/26f71ad376c4/launch-c846c0e0cbc6.min.js');
-// } else if (window.location.host.endsWith('.page')) {
-//  await loadScript('https://assets.adobedtm.com/f4211b096882/26f71ad376c4/launch-6367a8aeb307-staging.min.js');
-// }
