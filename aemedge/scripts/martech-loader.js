e15cf7794aee4fe162804f2c9840a7ba719ca759
Kiran Murugulla <murugull@adobe.com>
2024-12-10 13:49:38 -0500

    adding datastreamid and ims org id for dev

--- aemedge/scripts/martech-loader.js
+++ aemedge/scripts/martech-loader.js
@@ -1,4 +1,4 @@
-import { getMetadata } from './aem.js';
+/* eslint-disable max-len */
 /* eslint-disable no-underscore-dangle */
 import {
   initMartech,
@@ -9,41 +9,29 @@ import {
   // eslint-disable-next-line import/no-relative-packages, import/no-unresolved
 } from '../plugins/martech/src/index.js';
 
-const DEFAULT_ALLOY_CONFIG = {
-  orgId: 'BC401D6765FB06150A495FF7@AdobeOrg',
-
-  // FIXME: remove this once OneTrust is implemented
-  defaultConsent: 'in',
-
-  onBeforeEventSend: (payload) => {
-    // ACDL is initialized in the lazy phase, so fetching from the JS array as a fallback during
-    // the eager phase
-    const dlState = window.adobeDataLayer.getState
-      ? window.adobeDataLayer.getState()
-      : window.adobeDataLayer[0];
-
-    payload.xdm = {
-      ...payload.xdm,
-      ...dlState,
-    };
-    if (payload.xdm.eventType === 'decisioning.propositionFetch'
-        && dlState._maruti.carInfo !== undefined
-        && dlState._maruti.carInfo.model !== undefined
-        && !Object.keys(payload.data.__adobe.target).length) {
-      payload.data.__adobe.target['user.categoryId'] = dlState._maruti.carInfo.model;
-    }
-  },
-  edgeConfigOverrides: {
-  },
-};
-
 const martechLoadedPromise = initMartech(
-  // The WebSDK config
-  // Documentation: https://experienceleague.adobe.com/en/docs/experience-platform/web-sdk/commands/configure/overview#configure-js
-  { ...DEFAULT_ALLOY_CONFIG, datastreamId: window.hlx?.alloyConfig?.profileEnabledDataStreamId },
+  {
+    datastreamId: 'b854d2aa-518d-4a36-9ff1-856c71d83ce2',
+    orgId: '9425401053CD40810A490D4C@AdobeOrg',
+    onBeforeEventSend: (payload) => {
+      payload.data.__adobe.target ||= {};
+
+      // set custom Analytics params
+      // see doc at https://experienceleague.adobe.com/en/docs/analytics/implementation/aep-edge/data-var-mapping
+      payload.data.__adobe.analytics ||= {};
+    },
+  },
   // The library config
   {
-    personalization: getMetadata('target'),
+    analytics: true, // whether to track data in Adobe Analytics (AA)
+    alloyInstanceName: 'alloy', // the name of the global WebSDK instance
+    dataLayer: true, // whether to use the Adobe Client Data Layer (ACDL)
+    dataLayerInstanceName: 'adobeDataLayer', // the name of the global ACDL instance
+    includeDataLayerState: true, // whether to include the whole data layer state on every event sent
+    launchUrls: ['https://assets.adobedtm.com/f4211b096882/1811238be96f/launch-088c475bb1c3-development.min.js'], // the list of Launch containers to load
+    personalization: true, // whether to apply page personalization from Adobe Target (AT) or Adobe Journey Optimizer (AJO)
+    performanceOptimized: true, // whether to use the agressive performance optimized approach or more traditional
+    personalizationTimeout: 1000, // the amount of time to wait (in ms) before bailing out and continuing page rendering
   },
 );
 
