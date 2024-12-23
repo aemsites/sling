095f12d7d77834e978128f9a6d445ed9dfc7be61
Charity Helms <chelms@adobe.com>
2024-12-23 17:04:56 -0500

    linting

--- aemedge/scripts/scripts.js
+++ aemedge/scripts/scripts.js
@@ -224,6 +224,12 @@ function createOptimizedBackgroundImage(section, breakpoints = [
   updateBackground();
 }
 
+/**
+ * consolidate the first two divs in a section into two columns
+ *
+ * @param main
+ */
+
 function makeTwoColumns(main) {
   const sections = main.querySelectorAll('.section.columns-2');
   sections.forEach((section) => {
@@ -238,34 +244,6 @@ function makeTwoColumns(main) {
     columnTarget.append(columnOne, columnTwo);
   });
 }
-
-/**
- * consolidate the offer boxes into one wrapper div
- *
- * @param main
- */
-/*
-function consolidateSectionColumns(main) {
-  const ob = main.querySelectorAll('.offer-box-wrapper');
-  let firstOB;
-  if (ob && ob.length > 1) {
-    ob.forEach((box, index) => {
-      if (index === 0) {
-        firstOB = box;
-      } else {
-        firstOB.append(...box.children);
-        box.remove();
-      }
-    });
-    if (main.querySelector('.section.offer-box-container.two-columns')) {
-      makeTwoColumns(main);
-    }
-  }
-}
-
- */
-
-
 
 /**
  * Finds all sections in the main element of the document
