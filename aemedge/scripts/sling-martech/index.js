f3589e591dcb8db2f3b2697c5241c23a6659f16c
Kiran Murugulla <murugull@adobe.com>
2025-01-27 15:58:11 -0500

    fix: the scope of analyticsInstance to window

--- aemedge/scripts/sling-martech/index.js
+++ aemedge/scripts/sling-martech/index.js
@@ -11,6 +11,6 @@ export const getAnalyticsInstance = getInstance;
 // setting the analytics instance on window object
 (() => {
     console.log(`instantiate analtics instance `)
-    window.sling.anlyticsInstance = getInstance('sling');
+    window.anlyticsInstance = getInstance('sling-martech');
 })();
 
