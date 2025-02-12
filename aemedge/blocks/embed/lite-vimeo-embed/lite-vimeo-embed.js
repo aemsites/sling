ae2ef4e79725f89cb6a9e486ebe7a7c57d4dbc37
Ritwik Srivastava <45959816+RitwikSrivastava@users.noreply.github.com>
2025-02-12 10:58:57 +0530

    Allow Fullscreen in Vimeo embed

--- aemedge/blocks/embed/lite-vimeo-embed/lite-vimeo-embed.js
+++ aemedge/blocks/embed/lite-vimeo-embed/lite-vimeo-embed.js
@@ -73,9 +73,10 @@ class LiteVimeo extends (globalThis.HTMLElement ?? class {}) {
     // No encoding necessary as [title] is safe. https://cheatsheetseries.owasp.org/cheatsheets/Cross_Site_Scripting_Prevention_Cheat_Sheet.html#:~:text=Safe%20HTML%20Attributes%20include
     iframeEl.title = this.playLabel;
     iframeEl.allow = 'accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture';
+    iframeEl.allowFullscreen = true;
     // AFAIK, the encoding here isn't necessary for XSS, but we'll do it only because this is a URL
     // https://stackoverflow.com/q/64959723/89484
-    iframeEl.src = `https://player.vimeo.com/video/${encodeURIComponent(this.videoId)}?autoplay=1`;
+    iframeEl.src = `https://player.vimeo.com/video/${encodeURIComponent(this.videoId)}?${params.toString()}`;
     this.append(iframeEl);
 
     // Set focus for a11y
